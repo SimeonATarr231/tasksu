436a49d46af14ff7573146e398662e7516ecf656
SimeonATarr231 <simeonatarr862@gmail.com>
2026-06-21 01:37:20 +0000

    Rename landing.html to index.html as homepage

--- server/index.js
+++ server/index.js
@@ -15,10 +15,6 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "../public")));
-// Serve landing page as homepage
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/landing.html"));
-});
 
 app.use(
   session({
